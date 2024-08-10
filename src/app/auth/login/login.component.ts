cf9a2f2444bba4f630b2e18f0183d372fa6152d9
EduardoGV1 <edu94sale11@gmail.com>
2024-08-10 17:59:29 -0500

    Login auth guard

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,23 +1,76 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { UsuarioService } from 'src/app/services/usuario.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: [ './login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor(private router : Router) { }
+  public formSubmitted = false;
 
-  ngOnInit(): void {
-  }
+  public loginForm : FormGroup;
+
+  constructor(
+    private router : Router,
+    private fb : FormBuilder,
+    private usuarioService : UsuarioService,
+  ) {
+    this.loginForm = this.fb.group({
+      email: [localStorage.getItem('emailProyAdd') || '',[Validators.required, Validators.minLength(3), Validators.email]],
+      password: ['',Validators.required],
+      remember : [false, Validators.required],
+    }
+  );
+   }
 
   login(){
+    this.formSubmitted=true;
     console.log('submit');
-    this.router.navigateByUrl('/');
+
+    this.usuarioService.login(this.loginForm.value).subscribe(
+      {
+        next: (resp) => {
+          console.log(resp);
+          if (this.loginForm.get('remember')?.value) {
+            localStorage.setItem('emailProyAdd',this.loginForm.get('email')?.value);
+          } else {
+            localStorage.removeItem('emailProyAdd')
+          }
+          Swal.fire({
+            title: "Good job!",
+            text: "You clicked the button!",
+            icon: "success"
+          }).then((result)=> {
+            this.router.navigateByUrl('/dashboard')
+          });
+        },
+        error: (error) => {
+          console.log(error.error.msg);
+          Swal.fire({
+            title: 'Error!',
+            text: error.error.msg,
+            icon: 'error',
+            confirmButtonText: 'Cool'
+          })
+        },
+        complete: () => console.log('completado')
+      }
+    )
+    
+    // this.router.navigateByUrl('/');
     
   }
 
-
+  campoNoValido(campo:string):boolean{
+    if (this.loginForm.get(campo)?.invalid && this.formSubmitted) {
+      return true;
+    } else { 
+      return false;
+    }
+  }
 }
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,15 +1,100 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UsuarioService } from 'src/app/services/usuario.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls:['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent{
 
-  constructor() { }
+  public formSubmitted = false;
 
-  ngOnInit(): void {
+  public registerForm = this.fb.group({
+    nombre: ['',[Validators.required, Validators.minLength(3)]],
+    email: ['',[Validators.required, Validators.minLength(3), Validators.email]],
+    password: ['',Validators.required],
+    password2: ['',Validators.required],
+    terminos:[false,Validators.required]
+  }, {
+    validators : this.passwordIguales('password','password2')
+  }
+);
+
+  constructor( 
+    private fb : FormBuilder,
+    private usuarioService : UsuarioService,
+    private router : Router
+  ) { }
+
+
+  crearUsuario(){
+    this.formSubmitted = true;
+    console.log(this.registerForm.valid);
+    if (this.registerForm.valid) {
+      this.usuarioService.crearUsuario(this.registerForm.value)
+      .subscribe({
+        next: (resp) => {
+          console.log(resp);
+          Swal.fire({
+            title: "Good job!",
+            text: "You clicked the button!",
+            icon: "success"
+          }).then((resp)=>{
+            console.log('resp register',resp);
+            this.router.navigateByUrl('/');
+            
+          });
+        },
+        error: (error) => {
+          console.log(error.error.msg);
+          Swal.fire({
+            title: 'Error!',
+            text: error.error.msg,
+            icon: 'error',
+            confirmButtonText: 'Cool'
+          })
+        },
+        complete: () => console.log('completado')
+      });
+    }
+    
   }
 
+  campoNoValido(campo:string):boolean{
+    if (this.registerForm.get(campo)?.invalid && this.formSubmitted) {
+      return true;
+    } else { 
+      return false;
+    }
+  }
+
+  aceptaTerminos(){
+    return !this.registerForm.get('terminos')?.value && this.formSubmitted;
+  }
+
+  passwordEquals(){
+    const pass1 = this.registerForm.get('password')?.value;
+    const pass2 = this.registerForm.get('password2')?.value;
+    if ((pass1 !== pass2) && this.formSubmitted) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  passwordIguales(password:string,password2:string){
+    return (formGroup : FormGroup) => {
+      const pass1Controls = formGroup.get(password);
+      const pass2Controls = formGroup.get(password2);
+      if (pass1Controls?.value === pass2Controls?.value) {
+        pass2Controls?.setErrors(null);
+      } else {
+        pass2Controls?.setErrors({noEsIgual:true})
+      }
+    }
+  }
 }
